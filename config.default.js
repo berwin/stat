e0299c4e2082d8ab7dcd48829b2409e0592c84c0
berwin <berwin1995@qq.com>
2015-03-12 14:00:46 +0800

    config.default

--- config.default.js
+++ config.default.js
@@ -3,7 +3,7 @@
 module.exports = {
     LISTEN : 9001,
     MONGO_RUL : 'mongodb://127.0.0.1:27017/stat',
-    MD5_SUFFIX : 'MD5_SUFFIX',
+    MD5_SUFFIX : 'Encryption',
     EMAIL : 'email',
     PASSWORD : 'password',
     USER_ID : 'userID'
