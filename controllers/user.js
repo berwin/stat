37a4def790621454616fb36d3dbcef3b0e2fc21e
berwin <berwin1995@qq.com>
2015-05-10 20:19:40 +0800

    active bug

--- controllers/user.js
+++ controllers/user.js
@@ -24,7 +24,7 @@ exports.signup = function (req, res) {
         var status = response.statusCode;
         if (status < 200 || status >= 300 && status !== 304) return done( userInfo.message );
 
-        var link = 'http://' + req.headers.host + '/activation?userID=' + userInfo._id;
+        var link = 'http://' + req.hostname + '/activation?userID=' + userInfo._id;
         var html = '<!doctype html><html><head><meta charset="UTF-8"><title>Stat</title><style>#mail{font:14px/200% "";background:#fff;}#mail a{display:inline-block;width:300px;height:60px;font:18px/60px "";background:#3498db;border-radius:5px;text-align:center;color:#fff;text-decoration:none;transition:.2s linear;}#mail a:hover{background:#5dade2;}#mail a:active{background:#2980B9;}</style></head><body><div id="mail"><h1>欢迎光临 Stat</h1><p>感谢您注册了Stat。验证您的电子邮件地址，请点击下面的链接。</p><a href="'+ link +'">邮箱验证</a><p>此链接永久有效。</p><p>快乐的统计数据</p><p>Berwin</p></div></body></html>';
 
         utils.sendMail(mail, '激活您的Stat账户', html, function (err) {
